4b9e240f642aa576c593bf56d4cf03006e1017ae
Morgan Christensen <mchr335@wgu.edu>
2023-06-08 14:37:55 -0500

    map.ts update functions

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../country';
+import { CountryService } from '../country.service';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements OnInit{
+  countries: Country[] = [];
+
+  constructor(private countryService: CountryService) { }
+
+  ngOnInit(): void {
+    this.getCountries();
+  }
+
+  getCountries(): void {
+    this.countryService.getCountries()
+    .subscribe(countries => this.countries = countries);
+  }
 
 }
